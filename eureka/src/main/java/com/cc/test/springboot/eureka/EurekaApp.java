b7d0f5a847bcbe1c591273a01b0e231cbb7c5d3c
Benjamin <Benjamin@Benjamin-PC>
2018-04-14 21:01:14 +0800

    init
    Change-Id: I4802c2d9c940b281534bb77ee533af1e52cc321c

---
+++ eureka/src/main/java/com/cc/test/springboot/eureka/EurekaApp.java
@@ -0,0 +1,20 @@
+package com.cc.test.springboot.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApp extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApp.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EurekaApp.class);
+    }
+}
