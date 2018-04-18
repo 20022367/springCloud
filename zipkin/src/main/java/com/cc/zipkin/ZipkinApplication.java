b70fc67ab38d8afce3d74d1aba8d2317577b74c5
Benjamin <Benjamin@Benjamin-PC>
2018-04-18 23:40:58 +0800

    fix

--- zipkin/src/main/java/com/cc/zipkin/ZipkinApplication.java
+++ zipkin/src/main/java/com/cc/zipkin/ZipkinApplication.java
@@ -3,7 +3,7 @@ package com.cc.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import zipkin.server.internal.EnableZipkinServer;
